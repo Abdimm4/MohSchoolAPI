20d73f0296462ccad5fc37e6799cc660ef18801f
Abdimm4 <abdimm4@augsburg.edu>
2026-09-16 19:55:36 -0500

    Update 4.0 Railway MySQL connection

--- database.py
+++ database.py
@@ -3,6 +3,12 @@ import mysql.connector
 
 
 def get_connection():
+    print("DEBUG MYSQLHOST:", repr(os.getenv("MYSQLHOST")))
+    print("DEBUG MYSQLPORT:", repr(os.getenv("MYSQLPORT")))
+    print("DEBUG MYSQLUSER:", repr(os.getenv("MYSQLUSER")))
+    print("DEBUG MYSQLDATABASE:", repr(os.getenv("MYSQLDATABASE")))
+    print("DEBUG ALL ENV KEYS:", [k for k in os.environ if "MYSQL" in k.upper()])
+
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
         port=int(os.getenv("MYSQLPORT", 3306)),
