02a4c725407c748e2782dfc45f33a0af6a1eb4f9
Abdimm4 <abdimm4@augsburg.edu>
2026-09-16 20:24:48 -0500

    Update 7.0 Railway MySQL connection

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
+        host=os.getenv("MYSQLHOST", "localhost"),
         port=int(os.getenv("MYSQLPORT", 3306)),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE")
+        user=os.getenv("MYSQLUSER", "root"),
+        password=os.getenv("MYSQLPASSWORD", ""),
+        database=os.getenv("MYSQLDATABASE", "MohSchoolAPI"),
     )
